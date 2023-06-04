ecf5d3f254c223cfe2e272d4f9510e08a56d3684
Mauricio Fow <mauriciofow@gmail.com>
2023-06-04 23:38:42 +0300

    1 initial setup

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import PromiseRouter from 'express-promise-router'
+import { eventsRouter } from './endpoints/events/router'
+
+export const routes = PromiseRouter()
+
+routes.use('/events', eventsRouter)
+routes.use('/tickets', eventsRouter)
